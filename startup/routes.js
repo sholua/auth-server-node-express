7407cc6c2f5021221abc54ee85e5cd308aec30a1
Oleksandr Shevchuk <s471652@gmail.com>
2021-01-12 13:42:30 +0200

    User authentication

--- startup/routes.js
+++ startup/routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const users = require("../routes/users");
 const error = require("../middleware/error");
+const auth = require("../routes/auth");
 
 module.exports = function (app) {
   app.use(cors());
   app.use(express.json());
   app.use("/api/users", users);
+  app.use("/api/auth", auth);
   app.use(error);
 };
