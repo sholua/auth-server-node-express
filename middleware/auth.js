074a06e5beae41c436feee4bb3292c92b51ca909
Oleksandr Shevchuk <s471652@gmail.com>
2021-01-12 22:31:32 +0200

    Remove requests file

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function (req, res, next) {
-  const accessToken = req.header("authorization");
+  const accessToken = req.header("Authorization");
   if (!accessToken)
     return res.status(401).send("Access denied. No token provided.");
 
